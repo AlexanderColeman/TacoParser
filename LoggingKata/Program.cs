9503b83b279ba98e825f6823c1f5e0fb3322360f
Alexander Coleman <alexanderjcoleman01@gmail.com>
2022-07-13 07:41:35 -0500

    working state

--- LoggingKata/Program.cs
+++ LoggingKata/Program.cs
@@ -36,6 +36,10 @@ namespace LoggingKata
             // TODO: Create two `ITrackable` variables with initial values of `null`. These will be used to store your two taco bells that are the farthest from each other.
             // Create a `double` variable to store the distance
 
+            ITrackable tacoBell1 = null;
+            ITrackable tacoBell2 = null;
+            double distance; 
+
             // Include the Geolocation toolbox, so you can compare locations: `using GeoCoordinatePortable;`
 
             //HINT NESTED LOOPS SECTION---------------------
